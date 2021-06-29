d19b135d50335a2a5b11249afef65f3a50a5654f
sclo00ps <louginescott@gmail.com>
2021-06-29 14:18:08 -0400

    seed, model, user file updates

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 //or charities
 const Charities = require('./Charities');
-const lgbt = require('./lbgt');
-const mentalHealth = require('./mentalHealth');
-const socialJustice = require('./socialJustice');
-const womensData = require('./womensData');
+//const lgbt = require('./lbgt');
+//const mentalHealth = require('./mentalHealth');
+//const socialJustice = require('./socialJustice');
+//const womensData = require('./womensData');
+const UserProfile = require('./userPrfofile');
 
 userProfile.hasMany(Charities, {
     foreignKey: 'charity_id',
